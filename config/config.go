2ec2948152f01be1e39a750b31e1ca98ecf97527
Jaime López <jlopez@magento.com>
2017-01-23 11:30:07 +0100

    Adds schedule option to configuration

--- config/config.go
+++ config/config.go
@@ -1,23 +1,25 @@
 package config
 
 import (
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
 )
 
 const (
-	DEFAULT_PURGE = true
-	DEFAULT_WARMUP = false
+	DEFAULT_PURGE       = true
+	DEFAULT_WARMUP      = false
 	DEFAULT_CONCURRENCY = 10
-	DEFAULT_BREAK = "0ms"
+	DEFAULT_BREAK       = "0ms"
 )
 
 type Config struct {
-	Sitemaps []string
-	Purge bool
-	Warmup bool
+	Sitemaps    []string
+	Purge       bool
+	Warmup      bool
 	Concurrency uint
-	Break string
+	Break       string
+	Schedule    string
 }
 
 func (config *Config) Parse(filename string) error {
@@ -31,11 +33,11 @@ func (config *Config) Parse(filename string) error {
 }
 
 func New(filename string) *Config {
-	config := &Config {
-		Purge: DEFAULT_PURGE,
-		Warmup: DEFAULT_WARMUP,
+	config := &Config{
+		Purge:       DEFAULT_PURGE,
+		Warmup:      DEFAULT_WARMUP,
 		Concurrency: DEFAULT_CONCURRENCY,
-		Break: DEFAULT_BREAK,
+		Break:       DEFAULT_BREAK,
 	}
 
 	error := config.Parse(filename)
